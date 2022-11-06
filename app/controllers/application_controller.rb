c11de6e6f0d48317240b8b79328ba1f29dc4f4a1
Timothy <lemayiant4@gmail.com>
2022-11-06 22:07:25 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
+  end
+  get '/games' do 
+    games = Game.all
+    games.to_json
+  end
+
+  get '/games/:id' do
+    
+    games = Game.find(params[:id])
+    games.to_json(include: { reviews: { include: :user } })
   end
 
 end
